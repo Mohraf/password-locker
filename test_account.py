2c70831160f69ebf3a66b5447e66a9a16262ffc2
mohraf <amosngok@gmail.com>
2022-01-26 13:58:55 +0300

    ignore py cache

--- test_account.py
+++ test_account.py
@@ -1,1 +1,22 @@
 import unittest
+from account import Account
+
+class TestAccount(unittest.TestCase):
+  '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+  '''
+  def setUp(self):
+    '''
+    Set up method to run before each test cases.
+    '''
+    self.new_account = Account("mohraf","Denime123") # create account object
+  
+  # def tearDown(self):
+  #   '''
+  #   tearDown method that does clean up after each test case has run.
+  #   '''
+  #   Contact.contact_list = []
+

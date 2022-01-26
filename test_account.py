1350aeacf1d925f642fca79a24cc2363a1636717
mohraf <amosngok@gmail.com>
2022-01-26 16:19:23 +0300

    add tear down method to save multiple account

--- test_account.py
+++ test_account.py
@@ -3,7 +3,7 @@ from account import Account
 
 class TestAccount(unittest.TestCase):
   '''
-    Test class that defines test cases for the contact class behaviours.
+    Test class that defines test cases for the account class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
@@ -14,11 +14,11 @@ class TestAccount(unittest.TestCase):
     '''
     self.new_account = Account("mohraf","Denime123") # create account object
   
-  # def tearDown(self):
-  #   '''
-  #   tearDown method that does clean up after each test case has run.
-  #   '''
-  #   Contact.contact_list = []
+  def tearDown(self):
+    '''
+    tearDown method that does clean up after each test case has run.
+    '''
+    Account.account_list = []
 
 
   def test_init(self):
@@ -32,12 +32,22 @@ class TestAccount(unittest.TestCase):
 
   def test_save_account(self):
     '''
-    test_save_contact test case to test if the contact object is saved into
-      the contact list
+    test_save_account test case to test if the account object is saved into
+      the account list
     '''
-    self.new_account.save_account() # saving the new contact
+    self.new_account.save_account() # saving the new account
     self.assertEqual(len(Account.account_list),1)
 
+
+  def test_save_multiple_account(self):
+    '''
+    test_save_multiple_account to check if we can save multiple account
+    objects to our account_list
+    '''
+    self.new_account.save_account()
+    test_account = Account("Test","user") # new account
+    test_account.save_account()
+    self.assertEqual(len(Account.account_list),2)
 
 if __name__ == '__main__':
   unittest.main()
